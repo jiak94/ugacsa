7abdb950ea0cb7c77b6f1e611e6044e1c0af72f6
Jiakuan Li <jiak94@uga.edu>
2014-10-18 23:03:42 -0400

    增加如果是用临时密码通过验证,则强制修改密码

--- admin/authentication.php
+++ admin/authentication.php
@@ -29,11 +29,20 @@
 				
 				$_SESSION['login']= 1;
 				$_SESSION['username'] = $username;
-				echo "<script> alert ('恭喜你登陆成功'); window.location.href = 'manage.php'</script>";
+				echo "<script>window.location.href = '../admin/manage.php'</script>";
 				exit();
 			}
+            else{
+                if($value["Username"]===$username && $value["tempPassword"] === $hashPassword){
+                    $_SESSION['login'] = 1;
+                    $_SESSION['username'] = $username;
+                    echo "<script>window.location.href = '../admin/forceChangePassword.php'</script>";
+                    exit();
+                }
+            }
 		}
 	}
 
-	echo "<script> alert ('用户名或密码错误,请重试'); window.location.href = 'login.php'</script>";
+
+	echo "<script> alert ('用户名或密码错误,请重试'); window.location.href = '../admin/login.php'</script>";
 ?>
