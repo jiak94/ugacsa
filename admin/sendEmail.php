cc22e829944f2a4eedb4a9d98a4e02c226438d7c
Jiakuan Li <jiak94@uga.edu>
2014-10-20 21:40:01 -0400

    重构了部分代码

--- admin/sendEmail.php
+++ admin/sendEmail.php
@@ -1,5 +1,7 @@
 <?php
 	require_once("../connect.php");
+    require_once("../inc/class/Email.php");
+    $EMAIL = new Email();
 	
 	function generatePassword($length=8)
 	{
@@ -27,24 +29,24 @@
 	$query = mysql_query($sql);
 
 	$data = mysql_fetch_assoc($query);
-	
+	$username = $data['Username'];
 	
 	$newPassword = generatePassword();
 	
 	$to = $data['Email'];
-	$subject = "Reset Password";
-	$message = "Your new password for username: ".$username." has been reset as ".$newPassword;
+	$subject = "密码重置";
+	$message = "你的账号: ".$username." 密码已经被重置为 ".$newPassword."\n请登录修改您的密码";
 	
 	if($data['Answer'] == $answer){
 		
 		$hashPassword = hash('md5', $newPassword);
 		
-		$updateSql = "Update Admin SET Password='$hashPassword' WHERE id=$id";
+		$updateSql = "Update Admin SET tempPassword='$hashPassword' WHERE id=$id";
 		
 		$query = mysql_query($updateSql);
 		
 		if($query){
-			mail($to, $subject, $message);
+			$EMAIL->sendEmailTo($to, $subject, $message);
 			echo "<script> alert('修改成功,请返回重新登录'); window.location.href = '../admin/login.php'</script>";
 			exit();
 		}
