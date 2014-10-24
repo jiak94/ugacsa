a2ec348f0c09a551f0fb5a27bcf43b57a5cc17db
Jiakuan Li <jiak94@uga.edu>
2014-10-23 20:48:31 -0400

    增加样式css样式

--- admin/forgetPassword.handle.php
+++ admin/forgetPassword.handle.php
@@ -11,7 +11,7 @@ $query = mysql_query($sql);
 
 
 if (!(mysql_num_rows($query))) {
-	echo "<script> alert('用户名不存在, 请重新注册'); window.location.href = 'createAdmin.php'</script> ";
+    echo "<script> alert('用户名不存在, 请重新注册'); window.location.href = 'createAdmin.php'</script> ";
 }
 
 
@@ -19,13 +19,13 @@ $data = mysql_fetch_assoc($query);
 
 
 if ($data['SecretQuestion'] == "firstCar") {
-	$secretQuestion = "What's the brand of your first car?";
+    $secretQuestion = "What's the brand of your first car?";
 } else if ($data['SecretQuestion'] == "firstPet") {
-	$secretQuestion = "What's the name of your first pet?";
+    $secretQuestion = "What's the name of your first pet?";
 } else if ($data['SecretQuestion'] == "motherLastName") {
-	$secretQuestion = "What's your mother's LAST name?";
+    $secretQuestion = "What's your mother's LAST name?";
 } else if ($data['SecretQuestion'] == "fatherFirstName") {
-	$secretQuestion = "What's your father's FIRST name?";
+    $secretQuestion = "What's your father's FIRST name?";
 }
 
 
@@ -33,18 +33,62 @@ if ($data['SecretQuestion'] == "firstCar") {
 <!DOCTYPE html>
 <html>
 <head>
-	<title>回答密保问题</title>
-	<meta charset="utf-8">
+    <title>回答密保问题</title>
+    <meta charset="utf-8">
+    <style type="text/css">
+        body {
+            margin: 0px;
+            min-width: 1100px;
+        }
+
+        .background {
+            height: auto;
+            width: auto;
+            z-index: -1;
+        }
+
+        .content {
+            width: 500px;
+            height: 260px;
+            margin: auto;
+            margin-top: 200px;
+            border-radius: 10px;
+            background: #f5f5f5;
+            border: solid red;
+            padding-top: 40px;
+        }
+        .form {
+            width: 150px;
+            margin: auto;
+        }
+
+        #answer {
+            height: 20px;
+            width: 150px;
+            border-radius: 5px;
+            font-size: medium;
+            padding-left: 10px
+        }
+        #button {
+            width: 50px;
+            margin-top: 10px;
+        }
+    </style>
 </head>
 <body>
-<h2>密保问题:</h2>
-<strong><?php echo($secretQuestion); ?></strong>
-<br>
-
-<form id="form" name="form" method="post" action="sendEmail.php">
-	<input type="hidden" name="id" id="id" value="<?php echo $data['id'] ?>">
-	<input id="answer" name="answer" placeholder="答案" required="true">
-	<input type="submit" id="button" name="button" value="确认">
-</form>
+<div class="background">
+    <div class="content">
+        <h3 align="center"><?php echo($secretQuestion); ?></h3>
+        <div class="form">
+            <form id="form" name="form" method="post" action="sendEmail.php">
+                <input type="hidden" name="id" id="id" value="<?php echo $data['id'] ?>">
+                <input id="answer" name="answer" placeholder="答案" required="true">
+                <input type="submit" id="button" name="button" value="确认">
+            </form>
+        </div>
+    </div>
+    <img src="../assets/img/loginBackground.jpg"
+         style="position:fixed;top: 0; bottom: 0; left: 0; right: 0; z-index: -1" height="100%" width="100%">
+</div>
 </body>
 </html>
--- admin/forgetPassword.php
+++ admin/forgetPassword.php
@@ -1,11 +1,69 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>忘记密码</title>
-	<meta charset="utf-8">
+    <title>忘记密码</title>
+    <meta charset="utf-8">
+    <style type="text/css">
+        body {
+            margin: 0px;
+            min-width: 1100px;
+        }
+
+        .background {
+            height: auto;
+            width: auto;
+            z-index: -1;
+        }
+
+        .content {
+            width: 500px;
+            height: 300px;
+            margin: auto;
+            margin-top: 200px;
+            border-radius: 10px;
+            background: #f5f5f5;
+            border: solid red;
+        }
+        .form {
+            width: 150px;
+            margin: auto;
+        }
+        .hint {
+            width: 190px;
+            margin: auto;
+            margin-top: 30px;
+        }
+        #username {
+            height: 20px;
+            width: 150px;
+            border-radius: 5px;
+            font-size: medium;
+            padding-left: 10px
+        }
+        #button {
+            width: 50px;
+            margin-top: 10px;
+        }
+    </style>
 </head>
-<form id="forgetPassword" name="forgetPassword" method="post" action="forgetPassword.handle.php">
-	<input id="username" name="username" placeholder="用户名" required="true">
-	<input type="submit" name="button" id="button" value="下一步">
-</form>
+<body>
+<div class="background">
+    <div class="content">
+        <h1 align="center">忘记密码</h1>
+
+        <div class="form">
+            <form id="forgetPassword" name="forgetPassword" method="post" action="forgetPassword.handle.php">
+                <input id="username" name="username" placeholder="用户名" required="true">
+                <input type="submit" name="button" id="button" value="下一步">
+            </form>
+        </div>
+        <div class="hint">
+            <strong>想起来了?</strong>
+            <a href="../admin/login.php"><strong>点击登录</strong></a>
+        </div>
+    </div>
+    <img src="../assets/img/loginBackground.jpg"
+         style="position:fixed;top: 0; bottom: 0; left: 0; right: 0; z-index: -1" height="100%" width="100%">
+</div>
+</body>
 </html>
