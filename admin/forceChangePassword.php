8b603f7cc646afad0ee41a4ec39a17081e080b41
Jiakuan Li <jiak94@uga.edu>
2014-10-24 10:24:58 -0400

    为页面增加了css样式

--- admin/forceChangePassword.php
+++ admin/forceChangePassword.php
@@ -6,48 +6,96 @@
  * Time: 1:28 AM
  */
 require_once("../connect.php");
-
+require_once("../inc/class/User.php");
 session_start();
+
 if ($_SESSION['login'] == 1) {
 
 } else {
-	echo "<script> alert ('您没有访问权限, 请登录'); window.location.href = '../admin/login.php'</script>";
-	exit();
+    echo "<script> alert ('您没有访问权限, 请登录'); window.location.href = '../admin/login.php'</script>";
+    exit();
 }
+
+$USER = new User($_SESSION['username']);
+$id = $USER->getMyId();
+
 ?>
 <html>
 <head>
-	<title>强制修改密码</title>
-	<meta charset="utf-8">
+    <title>强制修改密码</title>
+    <meta charset="utf-8">
+    <style type="text/css">
+        .background {
+            height: auto;
+            width: auto;
+            z-index: -1;
+        }
+        .forceModify {
+            width: 500px;
+            height: 300px;
+            margin: auto;
+            margin-top: 200px;
+            border-radius: 10px;
+            background: #f5f5f5;
+            border: solid red;
+        }
+        .form {
+            width: 150px;
+            margin: auto;
+        }
+        #newPassword {
+            height: 20px;
+            width: 150px;
+            border-radius: 5px;
+            font-size: medium;
+            padding-left: 10px
+        }
+        #secretQuestion, #newPasswordAgain, #answer, #email {
+            height: 20px;
+            width: 150px;
+            margin-top: 10px;
+            border-radius: 5px;
+            padding-left: 10px;
+        }
+        #button, {
+            width: 50px;
+            margin-top: 10px;
+        }
+    </style>
 </head>
 <body>
-<form id="forceChangePassword" method="post" action="../admin/forceChangePassword.handle.php">
-	<p>新密码:</p>
-	<input name="newPassword" type="password" required="true">
-	<br>
-
-	<p>再次输入新密码:</p>
-	<input name="newPasswordAgain" type="password" required="true">
-	<br>
-
-	<p>选择密保问题:</p>
-	<select name="secretQuestion" id="secretQuestion">
-		<option value="firstCar">What's the brand of your first car?</option>
-		<option value="firstPet">What's the name of your first pet?</option>
-		<option value="motherLastName">What's your mother's LAST name?</option>
-		<option value="fatherFirstName">What's your father's FIRST name?</option>
-	</select>
-	<br>
-
-	<p>密保答案:</p>
-	<input id="answer" name="answer" placeholder="密保答案" required="true">
-	<br>
-
-	<p>邮箱地址:</p>
-	<input id="email" name="email" pattern="([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}"
-	       required="true">
-	<br>
-	<input type="submit" id="button" name="button" value="submit">
-</form>
+<div class="background">
+    <div class="forceModify">
+        <h1 align="center">修改临时密码</h1>
+        <div class="form">
+            <form id="forceChangePassword" method="post" action="../admin/forceChangePassword.handle.php">
+                <input name="newPassword" id="newPassword" type="password" required="true" placeholder="新密码">
+                <br>
+                <input name="newPasswordAgain" id="newPasswordAgain" type="password" required="true" placeholder="确认密码">
+                <br>
+                <?php if($USER->secretQuestionSet($id)==false): ?>
+                <select name="secretQuestion" id="secretQuestion">
+                    <option value="firstCar">What's the brand of your first car?</option>
+                    <option value="firstPet">What's the name of your first pet?</option>
+                    <option value="motherLastName">What's your mother's LAST name?</option>
+                    <option value="fatherFirstName">What's your father's FIRST name?</option>
+                </select>
+                <br>
+                <input id="answer" name="answer" placeholder="密保答案" required="true">
+                <?php endif; ?>
+                <br>
+                <?php if($USER->getUserEmail($id)==NULL): ?>
+                <input id="email" name="email"
+                       pattern="([a-z0-9]*[-_]?[a-z0-9]+)*@([a-z0-9]*[-_]?[a-z0-9]+)+[\.][a-z]{2,3}"
+                       required="true" placeholder="Email">
+                <br>
+                <?php endif; ?>
+                <input type="submit" id="button" name="button" value="确认修改">
+            </form>
+        </div>
+    </div>
+    <img src="../assets/img/loginBackground.jpg"
+         style="position:fixed;top: 0; bottom: 0; left: 0; right: 0; z-index: -1" height="100%" width="100%">
+</div>
 </body>
 </html>
--- admin/modifyPassword.php
+++ admin/modifyPassword.php
@@ -4,39 +4,73 @@ session_start();
 if ($_SESSION['login'] == 1) {
 
 } else {
-	echo "<script> alert ('您没有访问权限, 请登录'); window.location.href = '../admin/login.php'</script>";
-	exit();
+    echo "<script> alert ('您没有访问权限, 请登录'); window.location.href = '../admin/login.php'</script>";
+    exit();
 }
-
-
 
 ?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>修改密码</title>
-	<meta charset="utf-8">
+    <title>修改密码</title>
+    <meta charset="utf-8">
+    <style type="text/css">
+        .background {
+            height: auto;
+            width: auto;
+            z-index: -1;
+        }
+        .modifyPassword {
+            width: 500px;
+            height: 300px;
+            margin: auto;
+            margin-top: 200px;
+            border-radius: 10px;
+            background: #f5f5f5;
+            border: solid red;
+        }
+        .form {
+            width: 150px;
+            margin: auto;
+        }
+        #username {
+            height: 20px;
+            width: 150px;
+            border-radius: 5px;
+            font-size: medium;
+            padding-left: 10px
+        }
+        #oldPassword, #newPassword, #renewPassword {
+            height: 20px;
+            width: 150px;
+            margin-top: 10px;
+            border-radius: 5px;
+            padding-left: 10px;
+        }
+    </style>
 </head>
 <body>
-<h1>修改密码</h1>
+<div class="background">
+    <div class="modifyPassword">
+        <h1 align="center">修改密码</h1>
 
-<form id="form" name="form" method="post" action="../admin/modifyPassword.handle.php">
-	<h3>用户名</h3>
-	<input id="username" name="username" required="true" placeholder="用户名">
-	<br>
+        <div class="form">
+            <form id="form" name="form" method="post" action="../admin/modifyPassword.handle.php">
 
-	<h3>旧密码</h3>
-	<input id="oldPassword" name="oldPassword" required="true" type="password">
-	<br>
-
-	<h3>新密码</h3>
-	<input id="newPassword" name="newPassword" required="true" type="password">
-	<br>
-
-	<h3>再次输入新密码</h3>
-	<input id="renewPassword" name="renewPassword" required="true" type="password">
-	<br>
-	<input type="submit" id="button" name="buttion">
-</form>
+                <input id="username" name="username" required="true" placeholder="用户名">
+                <br>
+                <input id="oldPassword" name="oldPassword" required="true" type="password" placeholder="旧密码">
+                <br>
+                <input id="newPassword" name="newPassword" required="true" type="password" placeholder="新密码">
+                <br>
+                <input id="renewPassword" name="renewPassword" required="true" type="password" placeholder="确认新密码">
+                <br>
+                <input type="submit" id="button" name="buttion">
+            </form>
+        </div>
+    </div>
+    <img src="../assets/img/loginBackground.jpg"
+         style="position:fixed;top: 0; bottom: 0; left: 0; right: 0; z-index: -1" height="100%" width="100%">
+</div>
 </body>
 </html>
